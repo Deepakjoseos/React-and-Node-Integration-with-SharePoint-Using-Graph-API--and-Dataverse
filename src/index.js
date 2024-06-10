1eb59e99ccb87f21c581ae7dbfb16b2a664f9e86
Deepakjoseos <josedipu9@gmail.com>
2024-06-10 10:42:28 +0530

    adding backend files

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const cors = require('cors');
+
+const {
+  getFundingRequests,
+  getSharePointDocumentLocation,
+  uploadToSharePoint,
+  downloadFromSharepoint,
+  AddORUpdateDocumentType,
+  deleteFileFromSharePoint,
+  getSharePointAccessToken
+} = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+// Use body-parser middleware
+
+// Define routes
+app.post('/api/getsharePointAccessToken', getSharePointAccessToken);
+app.get('/api/fundingRequests/:name', getFundingRequests);
+app.get('/api/sharepointdocumentlocation/:name', getSharePointDocumentLocation);
+app.post('/api/uploadToSharePoint', uploadToSharePoint);
+app.get('/api/downloadfromSharepoint', downloadFromSharepoint);
+app.post('/api/updateDocumentType',AddORUpdateDocumentType);
+app.delete('/api/deleteFileFromSharePoint',deleteFileFromSharePoint);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
